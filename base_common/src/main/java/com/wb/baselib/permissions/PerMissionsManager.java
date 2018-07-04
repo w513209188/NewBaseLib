33d5053c18470e40a8f7a938d88027ae043cc7cf
王辉 <513209188@qq.com>
2018-07-04 15:59:23 +0800

    修复多个权限申请的时候出现多次回调

--- base_common/src/main/java/com/wb/baselib/permissions/PerMissionsManager.java
+++ base_common/src/main/java/com/wb/baselib/permissions/PerMissionsManager.java
@@ -1,10 +1,13 @@
 package com.wb.baselib.permissions;
 
 import android.app.Activity;
+import android.util.Log;
 
 import com.tbruyelle.rxpermissions2.Permission;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 import com.wb.baselib.permissions.interfaces.PerMissionCall;
+
+import io.reactivex.functions.Consumer;
 
 
 public class PerMissionsManager {
@@ -26,23 +29,16 @@ public class PerMissionsManager {
      */
     public void getUserPerMissions(Activity mActivity,final PerMissionCall missionCall, final String... permissions){
         RxPermissions rxPermissions = new RxPermissions(mActivity);
-        rxPermissions.requestEach(permissions)
-               .subscribe(new io.reactivex.functions.Consumer<Permission>() {
-                   @Override
-                   public void accept(Permission permission) throws Exception {
-                       if (permission.granted) {
-                           // 用户已经同意该权限
-                           missionCall.userPerMissionStatus(true);
-                       } else if (permission.shouldShowRequestPermissionRationale) {
-                           // 用户拒绝了该权限，没有选中『不再询问』（Never ask again）,那么下次再次启动时，还会提示请求权限的对话框
-                           missionCall.userPerMissionStatus(false);
-                       } else {
-                           // 用户拒绝了该权限，并且选中『不再询问』
-                           missionCall.userPerMissionStatus(false);
-                       }
-                   }
-               });
-
+        rxPermissions.request(permissions).subscribe(new Consumer<Boolean>() {
+            @Override
+            public void accept(Boolean aBoolean) throws Exception {
+                if(aBoolean){
+                    missionCall.userPerMissionStatus(true);
+                }else {
+                    missionCall.userPerMissionStatus(false);
+                }
+            }
+        });
 
     }
 }
